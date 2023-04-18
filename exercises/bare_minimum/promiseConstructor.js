a8a90793e0eb2b0da019f1583b728fd3cf2474e4
Maximus Chen <Mr.MaximusChen@gmail.com>
2023-04-18 16:10:48 -0700

    Complete bare_minimum/promiseConstructor.js exercises.

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -7,15 +7,39 @@
 var fs = require('fs');
 var request = require('needle');
 var Promise = require('bluebird');
+const https = require('https');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+var pluckFirstLineFromFileAsync = function (filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err = null, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        var firstLine = data.split('\n')[0];
+        resolve(firstLine);
+      }
+    });
+  });
 };
+
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  return new Promise((resolve, reject) => {
+    if (url.slice(0, 8) !== 'https://') {
+      reject(new Error('Invalid URI'));
+    } else {
+      https.get(url, (res) => {
+        const { statusCode } = res;
+        if (statusCode === 404) {
+          reject(new Error('Invalid URI'));
+        } else {
+          resolve(statusCode);
+        }
+      });
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
